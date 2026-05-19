b4c59fd0fdd9a3d809a5862e9ae21032a6e1fb04
BigOldAnchor <niobium06@gmail.com>
2026-05-19 14:32:46 -0300

    Fix comments and formatting in TRAB_1SEM.asm

--- TRAB_1SEM.asm
+++ TRAB_1SEM.asm
@@ -1,14 +1,14 @@
 .data
-    	entrada:  .asciiz "\nInsira um número (0 para sair): "
+    	entrada:  .asciiz "\nInsira um nÃºmero (0 para sair): "
     	fimPrograma:  .asciiz "\nPrograma encerrado.\n"
-    	ehPalindromo: .asciiz "O numero é palíndromo.\n"
-    	naoEh: .asciiz "O numero nao é palíndromo.\n"
-    	maisProximo: .asciiz "O palindromo mais proximo é: \n "
+    	ehPalindromo: .asciiz "O numero Ã© palÃ­ndromo.\n"
+    	naoEh: .asciiz "O numero nao Ã© palÃ­ndromo.\n"
+    	maisProximo: .asciiz "O palindromo mais proximo Ã©: \n "
     	erro: .asciiz "Insira valores maiores que 1 e menores que 10000.\n"
 .text
 Main:
 Enquanto:
-    	# Insira o número (0 para sair)
+    	# Insira o nÃºmero (0 para sair)
     	li $v0, 4
     	la $a0, entrada
     	syscall
@@ -20,47 +20,47 @@ Enquanto:
     	bge $t0, 10000, Erro	# or (t0 >= 10000) vai para o erro 
     	j VerificaPalindromo	# Segue o algoritmo 
 
-Erro:	# Se a entrada não esteve de acordo, mostra mensagem e volta ao loop principal.
+Erro:	# Se a entrada nÃ£o esteve de acordo, mostra mensagem e volta ao loop principal.
     	li $v0, 4
     	la $a0, erro
     	syscall
     	j Enquanto
 
 VerificaPalindromo:		
-    	add $s0, $t0, $zero		# s0 = número original 
-    	li $s1, 0				# s1 = número invertido
+    	add $s0, $t0, $zero		# s0 = nÃºmero original 
+    	li $s1, 0				# s1 = nÃºmero invertido
 
 LoopInverte:				
     	beq $s0, 0, Comparacao	# if (s0 == 0) vai para Comparacao
     	rem $s2, $s0, 10			# s2 = ultimo digito do s0
-    	mul $s1, $s1, 10			# s1 é multiplicado por 10
+    	mul $s1, $s1, 10			# s1 Ã© multiplicado por 10
     	add $s1, $s1, $s2			# s1 += s2 
-    	div $s0, $s0, 10			# s0 /= 10 (remove o último digito)
+    	div $s0, $s0, 10			# s0 /= 10 (remove o Ãºltimo digito)
     	j LoopInverte			
 
 Comparacao:
     	beq $t0, $s1, EhPalindromo	# if (t0 == s1) vai para EhPalindromo
-    	j NaoEhPalindromo		# Caso contrário, vai para NaoEhPalindromo
+    	j NaoEhPalindromo		# Caso contrÃ¡rio, vai para NaoEhPalindromo
 
 EhPalindromo:
-	# O número é palíndromo
-	li $v0, 4
+		# O nÃºmero Ã© palÃ­ndromo
+		li $v0, 4
     	la $a0, ehPalindromo
     	syscall
     	j Enquanto 			# Volta ao loop
 
-NaoEhPalindromo:	# Inicia processo de procurar o palíndromo mais próximo
+NaoEhPalindromo:	# Inicia processo de procurar o palÃ­ndromo mais prÃ³ximo
     	li $v0, 4
-    	la $a0, naoEh		# Imprime mensagem de que o número inserido não é palíndromo.
+    	la $a0, naoEh		# Imprime mensagem de que o nÃºmero inserido nÃ£o Ã© palÃ­ndromo.
     	syscall
 	add $s4, $t0, $zero	# s4 = entrada
 
 BuscaPalindromo: 	# Loop que ocorre inversamente para busca
 	subi $s4, $s4, 1		# s4 -= 1
-   	add $s0, $s4, $zero	# s0 = número original
-    	li $s1, 0				# s1 = número invertido
+   	add $s0, $s4, $zero	# s0 = nÃºmero original
+    	li $s1, 0				# s1 = nÃºmero invertido
 
-LoopBusca:			# A lógica é a mesma do LoopInverte...
+LoopBusca:			# A lÃ³gica Ã© a mesma do LoopInverte...
     	beq $s0, 0, ComparaBusca
     	rem $s2, $s0, 10
     	mul $s1, $s1, 10
@@ -69,16 +69,16 @@ LoopBusca:			# A lógica é a mesma do LoopInverte...
    	j LoopBusca
 
 ComparaBusca:
-    	beq $s4, $s1, ImprimeProximo 	# if (s4 == s1) vai para ImprimePróximo
-    	j BuscaPalindromo				# Caso contrário, retorna ao loop de busca
+    	beq $s4, $s1, ImprimeProximo 	# if (s4 == s1) vai para ImprimePrÃ³ximo
+    	j BuscaPalindromo				# Caso contrÃ¡rio, retorna ao loop de busca
 
 ImprimeProximo: 
-	# O número palíndromo mais próximo é:
+	# O nÃºmero palÃ­ndromo mais prÃ³ximo Ã©:
     	li $v0, 4
     	la $a0, maisProximo
     	syscall
     	li $v0, 1
-    	add $a0, $s4, $zero #	Número palíndromo mais próximo.
+    	add $a0, $s4, $zero #	NÃºmero palÃ­ndromo mais prÃ³ximo.
     	syscall
     	j Enquanto
 
